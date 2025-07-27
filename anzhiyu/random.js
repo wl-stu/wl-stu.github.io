a735567c1573497a9af905e67640477931584561
wl-stu <wlstudy2024@163.com>
2025-07-28 03:58:54 +0800

    Site updated: 2025-07-28 03:58:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/26/Linux/","2025/07/26/hello-world/"];function toRandomPost(){
+var posts=["2025/07/26/hello-world/","2025/07/26/Linux/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
